d8e17fdbc5ab427bf5d5a13f184ce80a54092ae8
jurgennikolai <jurgennikolai@gmail.com>
2024-03-20 11:17:02 -0500

    first commit

---
+++ win_powershell/main.ps1
@@ -0,0 +1,28 @@
+﻿
+$rootPath = 'C:\directory\of\your\script'
+$listSRVs = Get-Content -Path "$rootPath\servers.txt";
+$pathCSV = "$rootPath\output.csv";
+
+New-Item -Path $pathCSV -ItemType File -Force -Value "Hostname,Status,Comment`n";
+
+foreach($srv in $listSRVs)
+{
+    Write-Output "* Host >>> $srv";
+    try
+    {
+        $listStatus = Invoke-Command -ComputerName $srv -ScriptBlock {
+            (ipconfig /all) | Select-String "IPv6"
+        } -ErrorAction Stop
+
+        $listStatusAsString = $listStatus -join ";"
+
+        Add-Content -Path $pathCSV -Value "$srv,$listStatusAsString,Ok";
+    }
+    catch
+    {
+        $errTypeName = $_.Exception.GetType().Name ;
+        
+        Add-Content -Path $pathCSV -Value "$srv,,$errTypeName";
+    }
+   
+}
